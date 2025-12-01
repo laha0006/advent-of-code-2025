a13fd52584ef6aaac05a3590b659000a865313eb
Lars <144362461+laha0006@users.noreply.github.com>
2025-12-01 18:27:55 +0100

    day 1 part 1

--- 1/1.py
+++ 1/1.py
@@ -1,1 +1,26 @@
-print("hello")
+data = open("input.txt", "r")
+
+lines = [line.strip() for line in data]
+ins = [(line[0],int(line[1::])) for line in lines]
+
+count = 0
+dial = 50
+
+def turn_dial(instruction):
+    global dial
+    global count
+    dir = instruction[0]
+    rotation = instruction[1]
+    if dir == "L":
+        turn = dial - rotation
+        dial = turn % 100
+    elif dir == "R":
+        turn = dial + rotation
+        dial = turn % 100
+    if dial == 0:
+        count += 1
+
+for i in ins:
+     turn_dial(i)
+print(count)
+
